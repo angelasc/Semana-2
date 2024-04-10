1ec554e957d56862f74116732a8c8247c616bdd8
Angela <a.souzacolombo@gmail.com>
2024-04-09 22:09:58 -0300

    Ajuste final

--- exercicio3-semana2.py
+++ exercicio3-semana2.py
@@ -1,12 +1,3 @@
-##Exercício 3 (Questão 3)
-#Faça um programa, com uma função que necessite de três argumentos, e que forneça a soma desses três argumentos.
-
-#Passos
-  #Definir 3 argumentos (3 números) para a função
-  #Criar a função de Soma usando os argumentos definidos como parâmetros
-  #Fazer a chamada da função
-
-
 number1 = int(input("Escreva o primeiro número: "))
 number2 = int(input("Escreva o segundo número: "))
 number3 = int(input("Escreva o terceiro número: "))
